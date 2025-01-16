4b8b318f1a8e69be6d21bd96171df0fb3e62a205
MH1214 <107912211+MH1214@users.noreply.github.com>
2025-01-16 00:20:48 -0800

    adding new wrist subsystem

---
+++ other_robots/crank_rewritten_sim/robot/subsystems/wrist.py
@@ -0,0 +1,15 @@
+import math
+
+import wpilib
+from commands2 import Subsystem
+from wpilib import SmartDashboard
+import rev
+
+import constants
+
+class Wrist(Subsystem):
+    def __init__(self):
+        super().__init__()
+
+    def move_wrist():
+
