5a7992395ee9761ace408695b1dd8c69d43a7580
MH1214 <107912211+MH1214@users.noreply.github.com>
2025-01-17 16:05:56 -0800

    adding wrist animation

--- other_robots/crank_rewritten_sim/robot/constants.py
+++ other_robots/crank_rewritten_sim/robot/constants.py
@@ -50,42 +50,42 @@ class ElevatorConstants:
             "shoulder_pivot": 180,
             "elbow_pivot": 180, 
             "wrist_pivot": 0,
-            "wrist_color": wpilib.Color8Bit("blue")
+            "wrist_color": wpilib.Color8Bit("#0000FF") #blue
         },
         "ground": {
             "elevator_height": 22,
             "shoulder_pivot": 135,
             "elbow_pivot": 120,
             "wrist_pivot": 0,
-            "wrist_color": wpilib.Color8Bit("blue")
+            "wrist_color": wpilib.Color8Bit("#0000FF")
         },
         "l1": {
             "elevator_height": 22,
             "shoulder_pivot": 150, #angle between the vertical and the shoulder ligament
             "elbow_pivot": 145, #angle between shoulder ligament and elbow ligament
             "wrist_pivot": 90, #angle between the horizontal and the wrist ligament
-            "wrist_color": wpilib.Color8Bit("blue")
+            "wrist_color": wpilib.Color8Bit("#0000FF")
         },
         "l2": {
             "elevator_height": 22,
             "shoulder_pivot": 180,
             "elbow_pivot": 150,
             "wrist_pivot": 90,
-            "wrist_color": wpilib.Color8Bit("red")
+            "wrist_color": wpilib.Color8Bit("#FF0000") #red
         },
         "l3": {
             "elevator_height": 38,
             "shoulder_pivot": 180,
             "elbow_pivot": 150, 
             "wrist_pivot": 90,
-            "wrist_color": wpilib.Color8Bit("red")
+            "wrist_color": wpilib.Color8Bit("#FF0000")
         },
         "l4": {
             "elevator_height": 38,
             "shoulder_pivot": 35, 
             "elbow_pivot": 270,
             "wrist_pivot": 90,
-            "wrist_color": wpilib.Color8Bit("red")
+            "wrist_color": wpilib.Color8Bit("#FF0000")
         },
         "processor": 0,
         "barge": 0,
