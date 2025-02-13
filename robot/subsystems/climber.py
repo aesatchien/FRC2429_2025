462e1273af59e3a54a3219f14934c53b58e133ad
Logan Quon <lquon@lacanadaengineeringclub.org>
2025-02-12 17:54:46 -0800

    CJH fixed problems with commits

--- robot/subsystems/climber.py
+++ robot/subsystems/climber.py
@@ -20,25 +20,31 @@ class Climber(Subsystem):
         self.controller = self.sparkmax.getClosedLoopController()
         self.config.softLimit.forwardSoftLimit(constants.ClimberConstants.k_climber_rotation_limit)
         self.config.softLimit.reverseSoftLimit(constants.ClimberConstants.k_climber_reverse_rotation_limit)
+        self.climber_abs_encoder.setInverted(True)
+
 
 
 
 
     # 3 Climber Positions:
     # Stowed, Ready, Climb
+    # Commit Issues with files right now.
     def rotate_motor_arms(self):
-        if self.is_stowed():
-            self.sparkmax.setReference(constants.ClimberConstants.k_climber_motor_voltage, SparkMax.ControlType)
+        if self.is_ready() or self.climb():
+            self.sparkmax.setReference(0, SparkMax.ControlType)
         else:
-            self.sparkmax.setReference(-1 * (constants.ClimberConstants.k_climber_motor_voltage, SparkMax.ControlType))
-
+            self.sparkmax.setReference(constants.ClimberConstants.k_climber_motor_voltage, SparkMax.ControlType)
 
     def is_stowed(self):
+        return self.climber_position == constants.ClimberConstants.k_climber_motor_stowed_angle
+
+    def is_ready(self):
         return self.climber_position == constants.ClimberConstants.k_climber_motor_rotation
 
+    def climb(self):
+        return (self.climber_position == constants.ClimberConstants.k_climber_motor_rotation
+                + constants.ClimberConstants.k_climber_motor_climber_reference_angle)
 
-    #def
-     #   pass
 
 
 
