77ce6dd1d651fc36942ee436116ee6fab86e1fc9
L3eC <leoslegos1@gmail.com>
2025-03-28 17:03:08 -0700

    l2 lower as well

--- robot/constants.py
+++ robot/constants.py
@@ -66,7 +66,7 @@ k_positions = {
         "wrist_color_for_setColor": wpilib.Color8Bit(255, 0, 0)
     },
     "l2": {
-        "elevator": 0.46, # 0.52 at ventura
+        "elevator": 0.44, # 0.52 at ventura
         "shoulder_pivot": math.radians(130),
         "wrist_pivot": math.radians(90),
         "wrist_color_for_ligament": wpilib.Color.kRed,
